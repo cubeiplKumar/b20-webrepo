8633327529bb1ed95ffd5e8dff6ce9c19188dc4a
cubeiplkumar <emailanilkumarm@gmail.com>
2023-06-06 19:22:58 +0530

    feature202 changes by shaik

--- Application.java
+++ Application.java
@@ -18,4 +18,14 @@ class Main {
     int result = obj.addNumbers(num1, num2);
     System.out.println("Sum is: " + result);
   }
+  # feature202 changes by Shaik on 23-Mar-2022
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+
+      // method call
+      int result = getSquare(i);
+      System.out.println("Square of " + i + " is: " + result);
+    }
+  }
+}
 }
